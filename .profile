f16f7ef078fbb8ac58078513be67ef3f990c03e8
Joanes Grandjean <22633767+grandjeanlab@users.noreply.github.com>
2023-02-23 20:16:34 +0100

    wayland gogogo

--- .profile
+++ .profile
@@ -1,2 +1,3 @@
-# launch startx
-startx
+export XDG_RUNTIME_DIR=/tmp/xdg-runtime-$(id -u)
+mkdir -p $XDG_RUNTIME_DIR
+dwl -s 'dwlb -font "monospace:size=16"'
--- install.sh
+++ install.sh
@@ -8,55 +8,40 @@ fi
 mkdir /home/gjoanes/.suckless
 cd /home/gjoanes/.suckless
 
-git clone https://git.sr.ht/~bt/dwm
-cd dwm
-echo "Installing dwm"
+git clone https://github.com/djpohly/dwl
+cd dwl
+echo "Installing dwl"
 make install
 cd ..
 
-git clone https://git.sr.ht/~bt/dmenu
-cd dmenu
-echo "Installing dmenu"
+git clone https://github.com/kolunmi/dwlb
+cd dwlb
+make
 make install
-cd ..
-
-git clone https://git.sr.ht/~bt/slstatus
-cd slstatus
-echo "Installing slstatus"
-make install
-cd ..
-
-git clone https://git.sr.ht/~bt/st
-cd st
-echo "Installing st"
-make install
-cd ..
-
-git clone https://git.sr.ht/~bt/surf
-cd surf
-echo "Installing surf"
-make install
-cd ..
-
-git clone https://git.sr.ht/~bt/slock
-cd slock
-echo "Installing slock"
-make install
-cd ..
-
 
 
 cd ..
+
+setup-devd udev
+
+adduser gjoanes input
+adduser gjoanes video
+adduser gjoanes audio
+
+rc-update add seatd
+rc-service seatd start
+adduser gjoanes seat
+
 
 rc-service dbus start
 rc-update add dbus
 
-rc-service networking stop
-rc-service wpa_supplicant stop
-rc-update del networking boot
-rc-update del wpa_supplicant boot
-rc-service networkmanager start
-rc-update networkmanager networkmanager
+#rc-service networking stop
+#rc-service wpa_supplicant stop
+#rc-update del networking boot
+#rc-update del wpa_supplicant boot
+#rc-service networkmanager start
+#rc-update networkmanager networkmanager
 
 rc-service alsa start
 rc-update add alsa
